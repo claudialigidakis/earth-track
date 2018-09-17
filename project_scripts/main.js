4e10226f02f91f44d87f16e32282513b3a286357
Claudia Ligidakis <30705905+claudialigidakis@users.noreply.github.com>
2018-09-16 18:17:22 -0700

    added marker, finished adding tiles and connecting tobackend

--- project_scripts/main.js
+++ project_scripts/main.js
@@ -1,3 +1,28 @@
 $(document).ready(function() {
   $('.materialboxed').materialbox();
 });
+
+$(document).ready(function(){
+  $('.scrollspy').scrollSpy()
+});
+
+
+let Markers = {
+    fn: {
+        addMarkers: function() {
+            const target = $('#image-wrapper');
+              $('<span class="marker"/>').css({
+                  top: 450,
+                  left: 450
+              }).appendTo(target);
+        },
+    },
+    init: function() {
+        this.fn.addMarkers();
+    }
+};
+
+$(function() {
+    Markers.init();
+
+});
--- project_scripts/nasa-images.js
+++ project_scripts/nasa-images.js
@@ -1,29 +1,67 @@
+const points = $('#points')
+const loader = document.querySelector('#loader')
+const map = document.querySelector('#map')
+
+
+const showSpinner = () => {
+  loader.style.display = 'block';
+  map.style.display = 'none';
+}
+
+const hideSpinner = () => {
+  loader.style.display = 'none';
+  map.style.display = 'block';
+}
+
+const quickLink = (event) => {
+  let values = event.currentTarget.id
+  let coordinates = values.split(",")
+  let latitude = coordinates[0].trim()
+  let longitude = coordinates[1].trim()
+  return createMap(latitude, longitude)
+}
+
+$(".quick").on("click", function(event){ quickLink(event); });
+
+
 $("#coordinates").submit(function(event) {
   event.preventDefault()
   let pointRegEx = new RegExp("^[-+]?[0-9]{1,3}(?:\.[0-9]{1,10})?$")
-  let coordinates = $("#points").val().split(",")
-  if (coordinates.length !== 2) return $('#points').addClass('invalid');
-
+  let coordinates = points.val().split(",")
+  if (coordinates.length !== 2) return points.addClass('invalid');
   let latitude = coordinates[0].trim()
   let longitude = coordinates[1].trim()
+  console.log(pointRegEx.test(latitude), pointRegEx.test(longitude));
   if (pointRegEx.test(latitude) && pointRegEx.test(longitude)) {
     return createMap(latitude, longitude)
   }
   else {
-    return $('#points').addClass('invalid');
+    return points.addClass('invalid');
   }
 });
 
-const createMap = (latitude, longitude) => {
-  $("#points").val("")
-  $('#points').removeClass('valid')
-  axios.post(`http://localhost:3000/views`, {latitude, longitude})
-  .then( data => {
-    const newImage = data.data
-    console.log(typeof newImage)
-    $('#map').attr("src", `${newImage}`)
-  })
-  .catch(err => {
-    return $('#points').addClass('invalid');
-  })
+const createMap = async (latitude, longitude) => {
+  try {
+    showSpinner()
+    points.val("")
+    points.removeClass('valid')
+    const images = await axios.post(`http://localhost:3000/views`, {latitude, longitude})
+    $('#display_points').html(`${latitude}, ${longitude}`)
+
+    $('#top-left').attr("src", `${images.data.top_left}`)
+    $('#top-middle').attr("src", `${images.data.top_middle}`)
+    $('#top-right').attr("src", `${images.data.top_right}`)
+    $('#center-left').attr("src", `${images.data.center_left}`)
+    $('#center').attr("src", `${images.data.center}`)
+    $('#center-right').attr("src", `${images.data.center_right}`)
+    $('#bottom-left').attr("src", `${images.data.bottom_left}`)
+    $('#bottom-center').attr("src", `${images.data.bottom_middle}`)
+    $('#bottom-right').attr("src", `${images.data.bottom_right}`)
+  }
+  catch {
+    return points.addClass('invalid');
+  }
+  finally {
+       hideSpinner()
+  }
 }
