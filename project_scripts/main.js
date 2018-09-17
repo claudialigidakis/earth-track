eaa6becca0e0f43b90d8bb412bab3b7bee1f3831
Claudia Ligidakis <30705905+claudialigidakis@users.noreply.github.com>
2018-09-17 11:23:30 -0700

    made all final changes

--- project_scripts/main.js
+++ project_scripts/main.js
@@ -2,27 +2,21 @@ $(document).ready(function() {
   $('.materialboxed').materialbox();
 });
 
-$(document).ready(function(){
+$(document).ready(function() {
   $('.scrollspy').scrollSpy()
 });
 
-
 let Markers = {
-    fn: {
-        addMarkers: function() {
-            const target = $('#image-wrapper');
-              $('<span class="marker"/>').css({
-                  top: 450,
-                  left: 450
-              }).appendTo(target);
-        },
-    },
-    init: function() {
-        this.fn.addMarkers();
+  fn: {
+    addMarkers: function() {
+      const target = $('#image-wrapper');
+      $('<span class="marker"/>').css({top: 450, left: 450}).appendTo(target);
     }
+  },
+  init: function() {
+    this.fn.addMarkers();
+  }
 };
-
 $(function() {
-    Markers.init();
-
+  Markers.init();
 });
--- project_scripts/nasa-images.js
+++ project_scripts/nasa-images.js
@@ -1,7 +1,5 @@
-const points = $('#points')
 const loader = document.querySelector('#loader')
 const map = document.querySelector('#map')
-
 
 const showSpinner = () => {
   loader.style.display = 'block';
@@ -27,8 +25,8 @@ $(".quick").on("click", function(event){ quickLink(event); });
 $("#coordinates").submit(function(event) {
   event.preventDefault()
   let pointRegEx = new RegExp("^[-+]?[0-9]{1,3}(?:\.[0-9]{1,10})?$")
-  let coordinates = points.val().split(",")
-  if (coordinates.length !== 2) return points.addClass('invalid');
+  let coordinates = $('#points').val().split(",")
+  if (coordinates.length !== 2) return $('#points').addClass('invalid');
   let latitude = coordinates[0].trim()
   let longitude = coordinates[1].trim()
   console.log(pointRegEx.test(latitude), pointRegEx.test(longitude));
@@ -36,15 +34,15 @@ $("#coordinates").submit(function(event) {
     return createMap(latitude, longitude)
   }
   else {
-    return points.addClass('invalid');
+    return $('#points').addClass('invalid');
   }
 });
 
 const createMap = async (latitude, longitude) => {
   try {
     showSpinner()
-    points.val("")
-    points.removeClass('valid')
+    $('#points').val("")
+    $('#points').removeClass('valid')
     const images = await axios.post(`http://localhost:3000/views`, {latitude, longitude})
     $('#display_points').html(`${latitude}, ${longitude}`)
 
@@ -59,7 +57,7 @@ const createMap = async (latitude, longitude) => {
     $('#bottom-right').attr("src", `${images.data.bottom_right}`)
   }
   catch {
-    return points.addClass('invalid');
+    return $('#points').addClass('invalid');
   }
   finally {
        hideSpinner()
